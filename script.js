aa8b116cd90f4211f5d13cdffc68e4a22aa980bd
Affekung <alfredskolde@gmail.com>
2026-06-17 20:52:32 +0200

    Enhance BMI calculation with category output
    Add BMI category classification to the calculation result.

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 function calculateBMI() {
 
-    let height = document.getElementById("height").value;
-    let weight = document.getElementById("weight").value;
+let height = document.getElementById("height").value;
+let weight = document.getElementById("weight").value;
 
-    height = height / 100;
+height = height / 100;
 
-    let bmi = weight / (height * height);
+let bmi = weight / (height * height);
 
-    document.getElementById("result").innerHTML =
-        "Ditt BMI är " + bmi.toFixed(1);
+let category = "";
+
+if (bmi < 18.5) {
+    category = "Undervikt";
+} else if (bmi < 25) {
+    category = "Normalvikt";
+} else if (bmi < 30) {
+    category = "Övervikt";
+} else {
+    category = "Fetma";
+}
+
+document.getElementById("result").innerHTML =
+    "Ditt BMI är " + bmi.toFixed(1) + "<br>" + category;
 
 }
